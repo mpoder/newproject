00d43bfc56fa55ebee9b5e150db93538ea3cceed
Mihkel Põder <mpoder@mihkelpc.laptop>
2016-09-15 13:22:50 +0300

    moar updates

--- login.php
+++ login.php
@@ -1,14 +1,22 @@
 <?php
-	//var_dump($_POST);
-	//does stuff exist?
-	$signupEmailError = "";
-	if (isset ($_POST["signupEmail"] ) ) {
-		//somebody PRESSED THE BUTTON
-		if (empty($_POST["signupEmail"])){
-			$signupEmailError = "Please enter your e-mail";		
-		}
+//var_dump($_POST);
+//does stuff exist?
+$signupEmailError = "";
+$signupPasswordError = "";
+if (isset ($_POST["signupEmail"] ) ) {
+	//somebody PRESSED THE BUTTON
+	if (empty($_POST["signupEmail"])){
+		$signupEmailError = "Please enter your e-mail";		
 	}
-
+}
+if (isset ($_POST["signupPassword"] ) ) {
+	if (empty($_POST["signupPassword"])){
+		$signupPasswordError = "Please enter a password.";
+	} else {
+		// pikkus vähemalt kaheksa
+		if (strlen ($_POST["signupPassword"]) < 8) { $signupPasswordError = "Please make sure your password is at least 8 characters long.";}
+	}
+}	
 ?>
 
 <!DOCTYPE html>
@@ -39,7 +47,7 @@
 		<br><br>
 		<label>Password</label>
 		<br>
-		<input name="signupPassword" type="password">
+		<input name="signupPassword" type="password"><?php echo $signupPasswordError; ?>
 		<br><br>
 		<input type="submit" value="Create user">
 		</form>
