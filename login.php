b68400b971637c8afc6de5860a713236b41bafb8
Mihkel Põder <mpoder@mihkelpc.laptop>
2016-09-15 12:36:01 +0300

    moar updates

--- login.php
+++ login.php
@@ -7,8 +7,10 @@
 
 		<h1>Log into the system</h1>
 		<form method="POST">
+		<label>E-mail address</label>
 		<input name="loginEmail" type="email">
 		<br><br>
+		<label>Password</label>
 		<input name="loginPassword" type="password">
 		<br><br>
 		<input type="submit" value="Log in">
@@ -16,8 +18,10 @@
 
 		<h1>Create a user</h1>
 		<form method="POST">
+		<label>E-mail address</label>
 		<input name="signupEmail" type="email">
 		<br><br>
+		<label>E-mail address</label>
 		<input name="signupPassword" type="password">
 		<br><br>
 		<input type="submit" value="Create user">
